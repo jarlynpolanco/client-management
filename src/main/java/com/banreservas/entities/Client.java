cc6e6f941d17afea314a55155e5e25463f105116
Jarlyn M. Polanco De la Rosa <jarlynpolanco@gmail.com>
2024-02-12 18:37:08 -0400

    Creating entity and repository

---
+++ src/main/java/com/banreservas/entities/Client.java
@@ -0,0 +1,89 @@
+package com.banreservas.entities;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Client extends PanacheEntity {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String secondSurname;
+    private String email;
+    private String address;
+    private String phone;
+    private String country;
+    private String demonym;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(final String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSecondSurname() {
+        return secondSurname;
+    }
+
+    public void setSecondSurname(final String secondSurname) {
+        this.secondSurname = secondSurname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(final String phone) {
+        this.phone = phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(final String country) {
+        this.country = country;
+    }
+
+    public String getDemonym() {
+        return demonym;
+    }
+
+    public void setDemonym(final String demonym) {
+        this.demonym = demonym;
+    }
+}
---
+++ src/main/java/com/banreservas/repositories/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.banreservas.repositories;
+
+import com.banreservas.entities.Client;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ClientRepository implements PanacheRepository<Client> {
+
+}
